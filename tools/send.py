9748743018eff27e4a6489228a367f96fdf8a9d0
egnor <egnor@ofb.net>
2021-08-31 23:03:26 -0700

    minor tweaks

--- tools/send.py
+++ tools/send.py
@@ -22,10 +22,9 @@ parser.add_argument("--msec", type=int, default=200, help="Frame time")
 device_args = parser.add_mutually_exclusive_group()
 device_args.add_argument("--address", help="MAC to address")
 device_args.add_argument("--code", help="Device code to find")
-show_args = parser.add_mutually_exclusive_group(required=True)
-show_args.add_argument("--packets", nargs="+", help="raw packets hex file")
-show_args.add_argument("--frames", nargs="+", help="animation images")
-show_args.add_argument("--glyphs", nargs="+", help="glyph images")
+parser.add_argument("--packets", nargs="+", help="raw packets hex file")
+parser.add_argument("--frames", nargs="+", help="animation images")
+parser.add_argument("--glyphs", nargs="+", help="glyph images")
 parser.add_argument("--mode", type=int, help="mode to set")
 parser.add_argument("--speed", type=int, help="speed to set")
 parser.add_argument("--brightness", type=int, help="brightness to set")
@@ -33,10 +32,11 @@ args = parser.parse_args()
 
 send_expect: List[Tuple[bytes, bytes]] = []
 
+print("=== Command(s) to send ===")
+
 if args.packets:
-    print(f"=== Loading packets ===")
     for packets_path in args.packets:
-        print(packets_path)
+        print(f"Raw packets: {packets_path}")
         with open(packets_path) as hex_file:
             for line in hex_file:
                 line = line.strip().replace(":", " ")
@@ -44,10 +44,9 @@ if args.packets:
                 send_expect.append(pair)
 
 if args.frames:
-    print(f"=== Loading frames ===")
     frames: List[PIL.Image.Image] = []
     for frame_path in args.frames:
-        print(frame_path)
+        print(f"Frame image: {frame_path}")
         frame = PIL.Image.open(frame_path).convert(mode="1")
         frames.append(frame.resize((48, 12)))
 
@@ -55,10 +54,9 @@ if args.frames:
     print()
 
 if args.glyphs:
-    print(f"=== Loading glyphs ===")
     glyphs: List[PIL.Image.Image] = []
     for glyph_path in args.glyphs:
-        print(glyph_path)
+        print(f"Glyph image: {glyph_path}")
         glyph = PIL.Image.open(glyph_path).convert(mode="1")
         new_width = glyph.size[0] * 12 // glyph.size[1]
         glyphs.append(glyph.resize((new_width, 12)))
@@ -67,18 +65,18 @@ if args.glyphs:
     print()
 
 if args.mode is not None:
-    print(f"=== Setting mode {args.mode} ===")
+    print(f"Set mode: {args.mode}")
     send_expect.extend(nametag.protocol.set_mode(args.mode))
 
 if args.speed is not None:
-    print(f"=== Setting speed {args.speed} ===")
+    print(f"Set speed: {args.speed}")
     send_expect.extend(nametag.protocol.set_speed(args.speed))
 
 if args.brightness is not None:
-    print(f"=== Setting brightness {args.brightness} ===")
+    print(f"Set brightness: {args.brightness}")
     send_expect.extend(nametag.protocol.set_brightness(args.brightness))
 
-print(f"{len(send_expect)} packets loaded")
+print(f"{len(send_expect)} packets" if send_expect else "No command requests")
 print()
 
 print("=== Connecting to nametag ===")
