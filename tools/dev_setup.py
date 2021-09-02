c32abfaba2ba59f5ab9bf3213f33d1647ebb57a2
Dan Egnor <egnor@ofb.net>
2021-09-01 17:43:50 -0700

    unified setup

---
+++ tools/dev_setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+from pathlib import Path
+from subprocess import check_call, check_output
+
+os.chdir(str(Path(__file__).parent.parent))
+startup_venv = os.environ.get("VIRTUAL_ENV")
+
+print("=== Update system packages (apt) ===")
+apt_packages = ["direnv", "libglib2.0-dev", "libjpeg-dev", "pipenv"]
+check_call(["sudo", "apt", "install"] + apt_packages)
+print()
+
+print("=== Update python virtualenv (pipenv) ===")
+check_call(["pipenv", "install"])
+check_call(["direnv", "allow"])
+venv_path = check_output(["pipenv", "--venv"], encoding="utf-8").strip()
+activate_path = Path(venv_path) / "bin" / "activate_this.py"
+exec(activate_path.open().read(), dict(__file__=str(activate_path)))
+print()
+
+print("=== Enable bluepy helper (setcap) ===")
+from bluepy.btle import helperExe
+check_call(["sudo", "setcap", "cap_net_admin=ep", helperExe])
+print()
+
+if startup_venv == venv_path:
+    print("::: Setup complete! :::")
+else:
+    print("*** Restart shell to activate virtualenv ***")
