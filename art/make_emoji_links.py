4d817b8d10109713aa6be393b7214db88f71de5b
Dan Egnor <egnor@ofb.net>
2021-09-14 18:03:19 -0700

    emoji links

---
+++ art/make_emoji_links.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+from pathlib import Path
+
+import emoji  # type: ignore
+
+twemoji_dir = Path(__file__).parent / "twemoji" / "assets"
+
+links_dir = Path(__file__).parent / "emoji_links"
+shutil.rmtree(links_dir)
+links_dir.mkdir()
+
+dicts = (emoji.UNICODE_EMOJI_ALIAS_ENGLISH, emoji.UNICODE_EMOJI["en"])
+for emoji, name in (item for d in dicts for item in d.items()):
+    hex = "-".join(f"{ord(ch):x}" for ch in emoji)
+    name = name.strip(":")
+    symlinks = [
+        (twemoji_dir / "72x72" / f"{hex}.png", links_dir / f"{name}.png"),
+        (twemoji_dir / "svg" / f"{hex}.svg", links_dir / f"{name}.svg"),
+    ]
+    for source, link_path in symlinks:
+        if source.exists():
+            relpath = Path(os.path.relpath(source, start=link_path.parent))
+            if not link_path.is_symlink():
+                link_path.symlink_to(relpath)
