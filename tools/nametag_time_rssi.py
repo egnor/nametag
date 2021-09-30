9954165f83555c5b0cf66927b9d86cffcd0eab8d
Dan Egnor <egnor@ofb.net>
2021-09-30 16:29:15 -0700

    fix nametag_time_rssi

--- tools/nametag_time_rssi.py
+++ tools/nametag_time_rssi.py
@@ -11,55 +11,38 @@ import PIL.Image  # type: ignore
 import PIL.ImageDraw  # type: ignore
 import PIL.ImageFont  # type: ignore
 
-import nametag.bluetooth
+import nametag.bluefruit
 import nametag.logging_setup
 import nametag.protocol
+import nametag.scanner
 
 fonts_dir = Path(__file__).parent.parent / "external" / "fonts"
 font_path = fonts_dir / "Spartan-Bold.ttf"
 loaded_font = PIL.ImageFont.truetype(str(font_path), 10)
 
 
-async def update_rssi(
-    conn: nametag.bluetooth.Connection, tag: nametag.bluetooth.ScanTag
-):
+async def update_rssi(tag: nametag.protocol.Nametag):
     image = PIL.Image.new("1", (48, 12))
     draw = PIL.ImageDraw.Draw(image)
     center_xy = (image.size[0] / 2, image.size[1] / 2)
 
-    text = f"{datetime.datetime.now().strftime('%H:%M')}{tag.rssi}"
+    text = f"{datetime.datetime.now().strftime('%H:%M')}{tag.dev.rssi}"
     draw.text(center_xy, text, fill=1, font=loaded_font, anchor="mm")
-    steps = nametag.protocol.show_frames([image], msec=1000)
-
     logging.info(f'[{tag.id}] Showing "{text}"')
-    await conn.do_steps(steps)
+    await tag.show_frames([image], msec=1000)
 
 
 async def run(args):
-    logging.info("Starting scanner...")
-    async with nametag.bluetooth.Scanner(
-        adapter=args.adapter, toggle_interval=args.toggle_interval
-    ) as scan:
-        while True:
-            scan.harvest_tasks()
-            visible = scan.tags
-            logging.info(f"Scanning: {' '.join(sorted(t.id for t in visible))}")
-            for tag in visible:
-                if tag.id in scan.tasks:
-                    logging.debug(f"[{tag.id}] Update in progress...")
-                elif not tag.rssi:
-                    logging.debug(f"[{tag.id}] No RSSI data...")
-                elif len(scan.tasks) >= 5:
-                    logging.debug(f"[{tag.id}] Waiting for others...")
-                else:
-                    logging.info(f"[{tag.id}] Connecting...")
-                    scan.spawn_connection_task(tag, update_rssi, tag)
-
-            await asyncio.sleep(1.0)
+    async with nametag.bluefruit.Bluefruit(port=args.port) as adapter:
+        options = nametag.scanner.ScannerOptions()
+        options.success_delay = 1.0
+        await nametag.scanner.scan_and_spawn(
+            adapter=adapter, runner=update_rssi, options=options
+        )
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--adapter", default="hci0", help="BT interface")
+parser.add_argument("--port", default="/dev/ttyACM0")
 parser.add_argument("--debug", action="store_true")
 parser.add_argument("--toggle_interval", type=float, default=10.0)
 args = parser.parse_args()
