4d14cb163cc7147c0f25b7ff504af4eaf29fc8e0
Dan Egnor <egnor@Anas-MacBook-Air-2.local>
2021-10-01 14:26:20 -0700

    emoji lowercase

--- external/make_emoji_links.py
+++ external/make_emoji_links.py
@@ -15,7 +15,7 @@ links_dir.mkdir()
 dicts = (emoji.UNICODE_EMOJI_ALIAS_ENGLISH, emoji.UNICODE_EMOJI["en"])
 for emoji, name in (item for d in dicts for item in d.items()):
     hex = "-".join(f"{ord(ch):x}" for ch in emoji)
-    name = name.strip(":")
+    name = name.lower().strip(":")
     symlinks = [
         (twemoji_dir / "72x72" / f"{hex}.png", links_dir / f"{name}.png"),
         (twemoji_dir / "svg" / f"{hex}.svg", links_dir / f"{name}.svg"),
