e0c07c52265e8a54df2d0a12e2837b61b483147d
Dan Egnor <egnor@ofb.net>
2021-10-02 11:53:12 -0700

    arduino updates

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -19,13 +19,18 @@ board_urls = "https://www.adafruit.com/package_adafruit_index.json"
 os.environ["ARDUINO_BOARD_MANAGER_ADDITIONAL_URLS"] = board_urls
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--cli", action="store_true")
-parser.add_argument("--setup", action="store_true")
 parser.add_argument("--show-properties", action="store_true")
-parser.add_argument("--upload", action="store_true")
 parser.add_argument("--fqbn", default="adafruit:nrf52:feather52832")
 parser.add_argument("--port", default="/dev/ttyUSB0")
+parser.add_argument("--programmer", default="nrfutil_boot")
 parser.add_argument("arg", nargs="*")
+
+commands = parser.add_mutually_exclusive_group(required=True)
+commands.add_argument("--burn-bootloader", action="store_true")
+commands.add_argument("--build", action="store_true")
+commands.add_argument("--cli", action="store_true")
+commands.add_argument("--setup", action="store_true")
+commands.add_argument("--upload", action="store_true")
 
 args = parser.parse_args()
 if args.cli:
@@ -36,23 +41,34 @@ if args.setup:
     run([cli_bin, "upgrade"], check=True)
     run([cli_bin, "core", "install", "adafruit:nrf52"], check=True)
 
-extra_flags = "-DSERIAL_BUFFER_SIZE=2048"
+if args.build or args.upload:
+    extra_flags = "-DSERIAL_BUFFER_SIZE=2048"
 
-command = [
-    str(cli_bin),
-    "compile",
-    f"--build-cache-path={arduino_dir / 'build_cache'}",
-    f"--build-path={arduino_dir / 'build'}",
-    f"--build-property=compiler.c.extra_flags={extra_flags}",
-    f"--build-property=compiler.cpp.extra_flags={extra_flags}",
-    f"--fqbn={args.fqbn}",
-    f"--output-dir={arduino_dir / 'build_output'}",
-    f"--port={args.port}",
-    f"--warnings=all",
-]
-if args.show_properties:
-    command.append("--show-properties")
-if args.upload:
-    command.append("--upload")
-command.extend(args.arg or [source_dir / "bluetalk"])
-run(command, check=True)
+    command = [
+        str(cli_bin),
+        "compile",
+        f"--build-cache-path={arduino_dir / 'build_cache'}",
+        f"--build-path={arduino_dir / 'build'}",
+        f"--build-property=compiler.c.extra_flags={extra_flags}",
+        f"--build-property=compiler.cpp.extra_flags={extra_flags}",
+        f"--fqbn={args.fqbn}",
+        f"--output-dir={arduino_dir / 'build_output'}",
+        f"--port={args.port}",
+        f"--warnings=all",
+    ]
+    if args.show_properties:
+        command.append("--show-properties")
+    if args.upload:
+        command.append("--upload")
+    command.extend(args.arg or [source_dir / "bluetalk"])
+    run(command, check=True)
+
+if args.burn_bootloader:
+    command = [
+        str(cli_bin),
+        "burn-bootloader",
+        f"--fqbn={args.fqbn}",
+        f"--port={args.port}",
+        f"--programmer={args.programmer}",
+    ]
+    run(command, check=True)
